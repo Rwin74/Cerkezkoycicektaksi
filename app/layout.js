0ae1113de84d17cd3ab52bc2ac3e1d72632681e8
unknown <atakan7496@gmail.com>
2026-07-24 10:10:55 +0300

    chore: add Google tag (gtag.js) tracking snippet

--- app/layout.js
+++ app/layout.js
@@ -122,6 +122,19 @@ export default function RootLayout({ children }) {
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
         />
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-16495430463"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'AW-16495430463');
+            `,
+          }}
+        />
       </head>
       <body className={outfit.className}>
         <AiOverview />
